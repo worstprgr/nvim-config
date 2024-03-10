e994cf8b664465c57c98b0c1cd2a59957f43660f
worstprgr <adam@seishin.io>
2024-03-11 00:05:39 +0100

    added yank system clibboard remap key

--- lua/worstprgr/remap.lua
+++ lua/worstprgr/remap.lua
@@ -12,3 +12,6 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<leader>s', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
+vim.keymap.set('n', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>cc', '"+yy')
+vim.keymap.set('v', '<leader>c', '"+y')
