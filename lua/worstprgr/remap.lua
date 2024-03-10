523a82d7821721f7c409bd2a5fa0ad41645e6201
worstprgr <adam@seishin.io>
2024-03-10 20:46:48 +0100

    adding a new remap for <Tab>

--- lua/worstprgr/remap.lua
+++ lua/worstprgr/remap.lua
@@ -12,3 +12,4 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<leader>s', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
+vim.keymap.set('i', '<Tab>', '    ')
